8e16c0f29bcbcefbd36f5a1da95d8803daa40172
kouhaku206 <carlosdsouza2000@gmail.com>
2024-10-02 16:52:20 -0400

    xd

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {'/': (_) => Telalogin(), 'telainicial': (_) => Telainicial()},
       initialRoute: '/',
+      routes: {'/': (_) => Telalogin(), 'telainicial': (_) => Telainicial()},
     );
   }
 }
